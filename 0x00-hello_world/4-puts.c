7e24c15c2e3fdecc3be76590a4b0fea9a5790ad2
olisemiracle <olisemiracle904@gmail.com>
2022-12-08 04:40:13 -0800

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main - This prints a text using puts
+ * Return: This returns 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
