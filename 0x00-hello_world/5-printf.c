ec01dd59a26a4e4065c61d1547346dc87f7483e7
olisemiracle <olisemiracle904@gmail.com>
2022-12-08 04:57:48 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - This prints a text using printf
+ * Return: This returns 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
