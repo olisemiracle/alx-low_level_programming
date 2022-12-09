6eb2fff140e9a8b0207e500e155e7e12d769118c
olisemiracle <olisemiracle904@gmail.com>
2022-12-09 05:47:54 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This prints alphabet
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch >= 'a' && ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+return (0);
+}
