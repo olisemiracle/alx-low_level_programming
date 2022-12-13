eb90f21d05f9bf78f6fd27b414ee04d79f525829
olisemiracle <olisemiracle904@gmail.com>
2022-12-13 00:26:23 -0800

    creating putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include "_putchar.c"
+/**
+ * main - Entry point
+ * Description: prints text
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar";
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,2 @@
+/* PROTOTYPE */
+void _putchar(char);
