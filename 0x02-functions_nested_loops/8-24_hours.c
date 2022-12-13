1534a16760922dd6bbd47a18b2c5242ea8cd6315
olisemiracle <olisemiracle904@gmail.com>
2022-12-13 07:28:33 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,22 @@
 #include "main.h"
-
+/**
+ * jack_bauer - prints 24 hours
+ * Return: nothing
+ */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int k, l;
 
-	for (i = 0; i <= 2; i++)
-	{
-		for (j = 0; j <= 3; j++)
-		{
-			for(k = 0; k <= 5; k++)
+			for (k = 0; k <= 23; k++)
 			{
-				for (l = 0; l <= 9; l++)
+				for (l = 0; l <= 59; l++)
 				{
-					_putchar(i + '0');
-					_putchar(j + '0');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
 					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
+					_putchar(l / 10 + '0');
+					_putchar(l % 10 + '0');
 					_putchar('\n');
 				}
 			}
-		}
-	}
 }
