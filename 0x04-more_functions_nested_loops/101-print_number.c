232a96dc939d92802938db35dd31e20b4803895b
olisemiracle <olisemiracle904@gmail.com>
2022-12-16 15:25:19 -0800

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,19 @@
 #include "main.h"
 
-#define TEN 10
-#define ZERO 0
-#define ONE 1
 /**
- * print_number - prints integers
+ * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
 void print_number(int n)
 {
-	int len = ZERO, digit, power, i, j, positive = n;
+	unsigned int num = n;
 
-	if (positive == ZERO)
-		_putchar('0');
-	else
+	if (n < 0)
 	{
-		if (positive < ZERO)
-		{
-			_putchar('-');
-			positive = positive * -ONE;
-		}
-		for (i = positive; i > ZERO; i = i / TEN)
-			len++;
-		len--;
-		for (i = len; len > ZERO; len--)
-		{
-			power = ONE;
-			for (j = ONE; j <= len; j++)
-				power = power * TEN;
-			digit = positive / power;
-			_putchar('0' + digit);
-			positive = positive - (digit * power);
-			if (len == ONE)
-				_putchar('0' + positive);
-		}
+		_putchar('-');
+		num = -num;
 	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
