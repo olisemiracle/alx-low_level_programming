c92e19ec6dbc786b44f3782fde4120c6dc72dda6
olisemiracle <olisemiracle904@gmail.com>
2022-12-19 02:57:08 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints the reverse of a string
+ * @s: pointer to string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	s--;
+	while (len >= 0)
+	{
+		_putchar(*s--);
+		len--;
+	}
+	_putchar('\n');
+}
