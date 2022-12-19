efd95386ac17533dea31b9f79275b564417704d2
olisemiracle <olisemiracle904@gmail.com>
2022-12-19 09:24:44 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _atoi - converts string to integer
+ * @s: string to be converted
+ * Return: integer value
+ */
+
+int _atoi(char *s)
+{
+	int i, number = 0, sign = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			sign = -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			number = (number * 10) + (s[i] - '0');
+	}
+	number = number * sign;
+	return (number);
+
+}
