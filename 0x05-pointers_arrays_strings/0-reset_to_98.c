8fba8860edace05cb56e64c3c4506a4914d1fff6
olisemiracle <olisemiracle904@gmail.com>
2022-12-18 23:09:12 -0800

    resets a value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Resets values to 98
+ * @n: pointer to a value
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is about pointers, array and string
