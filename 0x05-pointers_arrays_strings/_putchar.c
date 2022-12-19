50127775c79361f4f1aca344e65adc0f83fe3b5b
olisemiracle <olisemiracle904@gmail.com>
2022-12-19 01:12:21 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - prints a single character
+ * @c: character to be printed
+ * Return: nothing
+ */
+void _putchar(char c)
+{
+	putchar(c);
+}
