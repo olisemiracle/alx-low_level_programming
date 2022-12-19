de11ba4a5ef9ea847ed9c124f7c23b4dd6f2025c
olisemiracle <olisemiracle904@gmail.com>
2022-12-19 06:23:19 -0800

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - prints array elements
+ * @a: pointer to array
+ * @n: size of array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", a[i]);
+		i++;
+
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+			break;
+		}
+	}
+}
