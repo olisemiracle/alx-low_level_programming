9d32814cab150eebe6d291b14bf1540aa5923ced
olisemiracle <olisemiracle904@gmail.com>
2022-12-19 13:03:22 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,16 +7,31 @@
 
 int _atoi(char *s)
 {
-	int i, number = 0, sign = 1;
+		int i, n, m;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-			sign = sign * -1;
-		if (s[i] >= '0' && s[i] <= '9')
-			number = (number * 10) + (s[i] - '0');
-	}
-	number = number * sign;
-	return (number);
-
+		i = n = m = 0;
+		while (*(s + i) != '\0')
+		{
+			if (*(s + i) == '-')
+				m++;
+			if (*(s + i) >= '0' && *(s + i) <= '9')
+			{
+				while (*(s + i) >= '0' && *(s + i) <= '9')
+				{
+					if (n == 0)
+					{
+						n = (n * 10) + (*(s + i) - '0');
+						n *= -1;
+					}
+					else
+						n = (n * 10) - (*(s + i) - '0');
+					i++;
+				}
+				if (m % 2 == 0)
+					n *= -1;
+				return (n);
+			}
+			i++;
+		}
+		return (0);
 }
