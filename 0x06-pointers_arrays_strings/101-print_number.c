04177a03b99d3f625e186fd4cbb9fe4213ef60eb
olisemiracle <olisemiracle904@gmail.com>
2022-12-21 12:43:01 -0800

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,12 +11,15 @@ void print_number(int n)
 		_putchar('0');
 		return;
 	}
-	if (n < 0)
+	else
 	{
-		_putchar('-');
-		n = n * -1;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = n * -1;
+		}
+		if ((n / 10) != 0)
+			print_number(n / 10);
+		_putchar((n % 10) + '0');
 	}
-	if ((n / 10) != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
 }
