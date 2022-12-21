723919897cff62053d7317ee669e1a29bf5eb4a0
olisemiracle <olisemiracle904@gmail.com>
2022-12-21 04:18:25 -0800

    strupr

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * string_toupper - convert string to upper
+ * @str: source string
+ * Return: pointer to convert string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
