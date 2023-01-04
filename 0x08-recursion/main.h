bcb8a9b68909173f6f7ca0b34f36c6556eba9d5d
olisemiracle <olisemiracle904@gmail.com>
2023-01-04 08:42:05 -0800

    prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,4 +10,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int sqrt_check(int n, int c);
 int _sqrt_recursion(int n);
+int prime_check(int n, int f);
+int is_prime_number(int n);
 #endif /* MAIN_H */
