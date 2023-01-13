93412e929c8795e98b700e0bedeb909ea47b281d
olisemiracle <olisemiracle904@gmail.com>
2023-01-12 17:00:36 -0800

    calloc using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,12 +7,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	int *ptr;
+	char *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	p = (char *)ptr;
+	for (i = 0; i < nmemb * size; i++)
+		*(p + i) = 0;
 	return (ptr);
 }
