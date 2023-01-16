79f5ebe57ddaac38173f2c519a4cbd4a3ef81929
olisemiracle <olisemiracle904@gmail.com>
2023-01-15 23:01:22 -0800

    creates a struct called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - creates a struct named dog
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: name of dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
