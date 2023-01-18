8eed7848e57cbd2f838bd2117e50a68ed3360c61
olisemiracle <olisemiracle904@gmail.com>
2023-01-18 05:18:16 -0800

    fixing mistakes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,11 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		result = (cmp)(array[i]);
-		if (result != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			result = (cmp)(array[i]);
+			if (result != 0)
+				return (i);
+		}
 	}
 	return (-1);
 }
