d52c83e9c73de8e08b6ae1bff5f2ea6cf42b2654
olisemiracle <olisemiracle904@gmail.com>
2023-01-31 01:22:28 -0800

    finishing up

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,6 +8,8 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *current = *head;
 
+	if (head == NULL)
+		return (NULL);
 	while (current != NULL)
 	{
 		*head = current->next;
